7d3531a7ffbe20a9312b98ec92a7ffb26cd4f86a
davidlnowak <nowak.david@gmail.com>
2016-04-24 15:43:21 -0400

    Facial Recognition
    Working on code for facial recognition.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var capture;
+var tracker;
 
 function setup() {
   capture = createCapture(VIDEO);
@@ -7,9 +8,18 @@ function setup() {
 
   var c = createCanvas(400, 400);
   c.position(0, 0);
+
+  tracker = new clm.tracker();
+  tracker.init(pModel);
+  tracker.start(capture.elt);
 }
 
 function draw() {
+  var p = tracker.getCurrentPosition();
+  console.log(p);
+
+  fill('#FFFF00');
+  ellipse(p[62][0], p[62][1], 50, 50);
   //image(capture, 0, 0, width, width * capture.height / capture.width);
   //filter(INVERT);
 }
